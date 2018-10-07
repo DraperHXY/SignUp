026551e414b55831ad2c01adde76cd8d7b986bea
Draper_HXY <Draper_HXY@163.com>
2018-10-07 20:32:01 +0800

    log4j2 + 测试修改

--- test/java/com/draper/mapper/StudentMapperTest.java
+++ test/java/com/draper/mapper/StudentMapperTest.java
@@ -2,10 +2,15 @@ package com.draper.mapper;
 
 import com.draper.BaseTest;
 import com.draper.entity.Student;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -18,9 +23,9 @@ public class StudentMapperTest extends BaseTest {
     @Autowired
     private StudentMapper studentMapper;
 
-    @Test
+    @Before
     public void testInsertStudent() throws ParseException {
-        logger.debug("插入");
+        logger.warn("插入");
         logger.info("current time {}", new Date());
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         long time_long = sdf.parse("2018-10-2").getTime();
@@ -53,13 +58,13 @@ public class StudentMapperTest extends BaseTest {
 
     @Test
     public void testSelectByName() {
-        logger.debug("查找");
-        logger.info("current time {}", new Date());
+        logger.warn("查找");
         Student student = studentMapper.selectByName("贾伟奇");
     }
 
     @Test
     public void testUpdateWishByName() {
+        logger.warn("修改");
         studentMapper.updateWishByName("贾伟奇", "老大最帅！");
     }
 
@@ -86,35 +91,35 @@ public class StudentMapperTest extends BaseTest {
 //        }
 //    }
 
-    @Test
+    @After
     public void testDeleteStudentByOnlineId() {
-        logger.debug("删除");
+        logger.warn("删除");
         logger.info("current time {}", new Date());
         studentMapper.deleteItStudentByOnlineId(4975);
         studentMapper.deleteSimpleStudent(4975);
     }
 
-    @Test
-    public void testInsert1000Student() throws ParseException {
-        int id = 10000;
-        for (int i = 0; i < 1000; i++) {
-            logger.debug("插入第 {} 条数据", i);
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-            long time_long = sdf.parse("2018-10-2").getTime();
-            Student student = new Student();
-            student.setCreateAt(System.currentTimeMillis());
-            student.setOnlineId(id + i);
-            student.setStudyType("JAVA工程师");
-            student.setStartStudyTime(time_long);
-            student.setDailyLink("http://www.jnshu.com/school/86/daily");
-            student.setWish("如果我不能在IT特训营拼尽全力，为自己以后的修行路上打好基础，就让我变胖2k斤！");
-            student.setCoachSenior("桂伟腾");
-            student.setKnowFrom("知乎");
-            student.setName("贾伟奇");
-            student.setQq("860733414");
-            student.setGraduateSchool("北京YD大学");
-            studentMapper.insertItStudent(student);
-            studentMapper.insertSimpleStudent(student);
-        }
-    }
+//    @Test
+//    public void testInsert1000Student() throws ParseException {
+//        int id = 10000;
+//        for (int i = 0; i < 1000; i++) {
+//            logger.debug("插入第 {} 条数据", i);
+//            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+//            long time_long = sdf.parse("2018-10-2").getTime();
+//            Student student = new Student();
+//            student.setCreateAt(System.currentTimeMillis());
+//            student.setOnlineId(id + i);
+//            student.setStudyType("JAVA工程师");
+//            student.setStartStudyTime(time_long);
+//            student.setDailyLink("http://www.jnshu.com/school/86/daily");
+//            student.setWish("如果我不能在IT特训营拼尽全力，为自己以后的修行路上打好基础，就让我变胖2k斤！");
+//            student.setCoachSenior("桂伟腾");
+//            student.setKnowFrom("知乎");
+//            student.setName("贾伟奇");
+//            student.setQq("860733414");
+//            student.setGraduateSchool("北京YD大学");
+//            studentMapper.insertItStudent(student);
+//            studentMapper.insertSimpleStudent(student);
+//        }
+//    }
 }
